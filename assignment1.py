166404ab315bccca2a01a07cf289c8163b0e93b1
ZHANGYUDAN1222 <1009040705@qq.com>
2020-08-15 22:22:41 +0800

    create empty lists places, places_n and places_v, add corresponding lists into them, add user_input for menu in main()

--- assignment1.py
+++ assignment1.py
@@ -24,9 +24,24 @@ def csv_to_list(places,places_n,places_v):
     return places, places_n, places_v
 
 
+
 def main():
     print("Travel Tracker 1.0 - by <Yudan Zhang>")
+    places = []
+    places_v = []
+    places_n = []
 
+    places, places_n, places_v = csv_to_list(places, places_n, places_v)
+
+    print("Travel Tracker 1.0 - by <Yudan Zhang>")
+    places_n.sort(key=itemgetter(2))
+    places_v.sort(key=itemgetter(2))
+    menu = input('''Menu:
+L - List places
+A - Add new place
+M - Mark a place as visited
+Q - Quit
+>>> ''').upper()
 
 if __name__ == '__main__':
     main()
